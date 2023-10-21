c11cad51f4d0f7deceea1f9e3f16ed94b4b8b0a2
QAF Lab - Info <info@qaflab.com>
2023-10-21 23:45:51 +0300

    setup sign in next auth page

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authHandler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                try {
+                    return null
+                } catch (error) {
+                    throw new Error("error");
+
+                }
+            }
+        })
+    ]
+})
+
+export { authHandler as GET, authHandler as POST }
