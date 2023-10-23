71915e02931b561de6b683878dd1b4b9a0a3187f
QAF Lab - Info <info@qaflab.com>
2023-10-23 09:17:12 +0300

    fix login and register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <main className="flex h-screen">
       <div className="w-1/3 flex justify-center items-center">
-        <LoginForm />
+        <RegistrationForm />
       </div>
       <div className="w-2/3 bg-[#0f0f16] flex items-center justify-center">
         <img className="w-1/4 object-contain" src="/login.png" alt="" />
