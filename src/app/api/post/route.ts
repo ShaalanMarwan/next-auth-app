a2c9eb2db38652275f24f51a1442f7ee1231937d
QAF Lab - Info <info@qaflab.com>
2023-10-23 20:00:45 +0300

    verify jwt get posts page in api

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,18 +1,25 @@
+import { verifyJwt } from "@/helpers/jwt";
 import prisma from "@/helpers/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  try {
-    const posts = await prisma.post.findMany();
-    return NextResponse.json({ posts }, { status: 200 });
-  } catch (error: Error | any) {
-    console.error(error);
-    return NextResponse.json(
-      {
-        message: "Something went wrong while trying to load the posts",
-        result: error,
-      },
-      { status: 500 }
-    );
-  }
+    try {
+        const accessToken = request.headers.get("Authorization");
+        const decoded = verifyJwt(accessToken);
+
+        if (!accessToken || !decoded) {
+            return NextResponse.json({ message: "You are not authorized to get this data" }, { status: 401 });
+        }
+        const posts = await prisma.post.findMany();
+        return NextResponse.json({ posts }, { status: 200 });
+    } catch (error: Error | any) {
+        console.error(error);
+        return NextResponse.json(
+            {
+                message: "Something went wrong while trying to load the posts",
+                result: error,
+            },
+            { status: 500 }
+        );
+    }
 }
