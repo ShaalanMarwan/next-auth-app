141a2f248b3b3c0223d0cd9ac5591fe4915cc4a2
QAF Lab - Info <info@qaflab.com>
2023-10-23 16:51:08 +0300

    design home page with logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+import LogoutButton from "@/components/logout-button";
+
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main
+      className="bg-black flex flex-col justify-center items-center space-y-8 h-screen"
+    >
+      <h1
+        className="text-white text-center text-2xl font-bold uppercase"
+      >
+        You have logged in!
+      </h1>
+      <img
+        src="/treasure.png"
+        alt="treasure chest illustration"
+        className="w-1/4 object-contain"
+      />
+      <LogoutButton />
+    </main>
+  )
 }
