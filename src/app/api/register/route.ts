679b37388489ee5ac3b51ea09645d34209dcc6b3
QAF Lab - Info <info@qaflab.com>
2023-10-23 16:48:24 +0300

    refactor auth register code

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,36 +1,42 @@
 import prisma from "@/helpers/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import * as bcrypt from 'bcrypt';
 
-
-import { NextRequest, NextResponse } from "next/server"
-
-import * as bcrypt from "bcrypt"
-
+/**
+ * Handler for the POST request.
+ * @param {NextRequest} request - The incoming request object.
+ * @returns {NextResponse} - The response object.
+ */
 export async function POST(request: NextRequest) {
     try {
-        const { email, password } = await request.json()
+        // Retrieve the email and password from the request body
+        const { email, password } = await request.json();
 
-        if (!email || !password) return NextResponse.json({
-            message: "Email and password are required"
-        }, {
-            status: 400,
-        })
+        // Check if both email and password are provided
+        if (!email || !password) {
+            // Return a 400 Bad Request response with an error message
+            return NextResponse.json({ message: "Both fields are required" }, { status: 400 });
+        }
+
+        // Create a new user with the provided email and hashed password
         const user = await prisma.user.create({
             data: {
                 email: email.toLowerCase(),
-                password: bcrypt.hashSync(password, 10)
+                password: await bcrypt.hash(password, 10)
             }
-        })
+        });
+        
         const { password: hashedPassword, ...result } = user;
-        return NextResponse.json({ result, }, { status: 201 })
+        
+        // Return a 201 Created response with the result
+        return NextResponse.json({ result }, { status: 201 });
+
     }
-
     catch (e) {
-        console.error(e)
-        return NextResponse.json({
-            message: "Something went wrong"
-        }, {
-            status: 500,
-        })
-
+        // Log the error
+        console.error(e);
+        
+        // Return a 500 Internal Server Error response with an error message and the error object
+        return NextResponse.json({ message: "Something went wrong while trying to register", result: e }, { status: 500 });
     }
 }
