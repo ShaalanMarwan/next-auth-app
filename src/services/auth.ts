7224b20639f78b167d0ad71e4fe25b3e7609043b
QAF Lab - Info <info@qaflab.com>
2023-10-23 16:49:20 +0300

    add auth register api

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,15 +1,27 @@
-
+/**
+ * Registers a user with the given email and password.
+ * @param {string} email - The user's email.
+ * @param {string} password - The user's password.
+ * @returns {Promise<Object>} - A promise that resolves with the response JSON if successful, otherwise rejects with an error.
+ */
 export const registerUser = (email: string, password: string) => {
     return new Promise((resolve, reject) => {
-        fetch("/api/register", {
-            method: "post",
+        // Send a POST request to the '/api/register' endpoint
+        fetch('/api/register', {
+            method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: {
                 "Accept": "application/json",
+                "Content-Type": "application/json",
             }
-
-        }).then((res) => {
-            res.json().then((json) => resolve(json)).catch(e => reject(e))
-        }).catch(e => reject(e))
+        })
+            .then(res => {
+                // Parse the response JSON
+                res
+                    .json()
+                    .then(json => resolve(json))
+                    .catch(e => reject(e))
+            })
+            .catch(e => reject(e))
     })
 }
