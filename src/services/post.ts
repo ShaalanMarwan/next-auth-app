407a236fa81c521dcc62cc476ea880471d8cc20b
Shaalan Marwan <info@qaflab.com>
2023-10-24 10:15:59 +0300

    doc string for post service

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,3 +1,8 @@
+/**
+ * Retrieves all posts from the API.
+ * @param accessToken - The access token for authorization.
+ * @returns A promise that resolves to the JSON response of the API call.
+ */
 export const getAllPosts = (accessToken: string) => {
     return new Promise((resolve, reject) => {
         fetch('/api/post', {
