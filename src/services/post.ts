03f97d9c3369c8ca0f252f099e5c90d6fae19716
QAF Lab - Info <info@qaflab.com>
2023-10-24 10:06:03 +0300

    add access token while fetching all posts

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,10 +1,11 @@
-export const getAllPosts = async () => {
+export const getAllPosts = (accessToken: string) => {
     return new Promise((resolve, reject) => {
         fetch('/api/post', {
             method: 'GET',
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
+                "Authorization": accessToken
             },
         }).then((res) => {
             res.json().then((json) => resolve(json)).catch((err) => reject(err))
